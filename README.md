15bf7ecdb8874508403cb664069801e4b8504f05
Wolf Paulus <wolf@paulus.com>
2025-02-01 10:30:52 -0700

    Let's get this started

---
+++ README.md
@@ -0,0 +1,10 @@
+## Practice Repo
+
+1. Fork this repo
+1. Clone the repo to your machine
+1. Add a non-abstract method to the shape class.
+1. Add a python module that implements a sub-class of Shape
+    - using your ERAU username for the file name.
+    - E.g. if your email is `doej1@erau.edu` you create this file: `doej1.py`
+1. Commit and push your code.
+1. Create a pull request
---
+++ shape.py
@@ -0,0 +1,32 @@
+"""
+Demo Class for Shape
+"""
+from abc import ABC, abstractmethod
+
+
+class Shape:
+    """
+    Abstract class for Shape
+    """
+
+    def __init__(self, name):
+        self.name = name
+        print(f"Creating a {self.__class__.__name__} with name {self.name}")
+
+    @abstractmethod
+    def area(self):
+        """ Abstract method for area """
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        """ Abstract method for perimeter"""
+        pass
+
+    def __str__(self):
+        """ str representation of the shape """
+        return f"{self.name} with area {self.area()} and perimeter {self.perimeter()}"
+
+    def __eq__(self, other):
+        """ strage equality, but hey, override it if you want """
+        return self.area() == other.area() and self.perimeter() == other.perimeter()
